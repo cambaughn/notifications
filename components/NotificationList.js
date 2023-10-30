b49f283b966035bbab1958a2b79132053f4c2d2b
Cameron Baughn <cam.baughn@gmail.com>
2023-10-30 15:40:59 -0700

    Add date headings

--- components/NotificationList.js
+++ components/NotificationList.js
@@ -1,26 +1,38 @@
-import { StyleSheet, View } from 'react-native';
+import { Fragment } from 'react';
+import { View, Text } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
 import NotificationListItem from './NotificationListItem';
 
 export default function NotificationList({ notifications, markRead, respondToFriendRequest }) {
   return (
     <View style={styles.container}>
-      { notifications.map(notification => {
+      { notifications.map((notification, index) => {
         return (
-          <NotificationListItem notification={notification} markRead={markRead} respondToFriendRequest={respondToFriendRequest} key={notification.id} />
+          <Fragment key={notification.id}>
+            { notification.timeago !== notifications[index - 1]?.timeago &&
+              <Text style={styles.dateHeading}>{notification.timeago}</Text>
+            }
+            <NotificationListItem notification={notification} markRead={markRead} respondToFriendRequest={respondToFriendRequest} />
+          </Fragment>
         )
       })}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = EStyleSheet.create({
   container: {
     width: '100%',
     display: 'flex',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'flex-start',
     paddingTop: 30,
-    gap: 1,
     paddingBottom: 60,
+  },
+  dateHeading: {
+    color: '#8A8A8A',
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 20,
   },
 });
